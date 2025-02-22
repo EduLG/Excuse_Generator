084a56d164d3ebac23a48b0e417e3c0175e3f1ef
Eduardo Ladron de Guevara <eduladron@gmail.com>
2025-02-22 18:24:46 +0000

    code ready. Ready for testing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function excuseGen() {
+  var guilty = ["The pope ", "The president ", "Alf ", "Homer Simpson ", "Bob Marley "]
+  var action = ["broke ", "stole ", "threw away ", "destroyed ", "corrupted "]
+  var victim = ["my phone ", "the sport center ", "a brand new limousine ", "a piece of cake ", "your pants "]
+  var when = ["this morning ", "at noon ", "just about now ", "on 1946", "while your wedding "]
+  var how = ["while soaked in alcohol.", "being completely out of control.", "after drinking 35 coffees and an ice cream.", "being asleep (dont ask me how it happened).", "trying to do something funny."]
+
+  var pt1 = guilty[Math.floor(Math.random()) * guilty.length]
+  var pt2 = action[Math.floor(Math.random()) * action.length]
+  var pt3 = victim[Math.floor(Math.random()) * victim.length]
+  var pt4 = when[Math.floor(Math.random()) * when.length]
+  var pt5 = how[Math.floor(Math.random()) * how.length]
+
+  var excuse = pt1 + pt2 + pt3 + pt4 + pt5
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").textContent = excuseGen();
 };
